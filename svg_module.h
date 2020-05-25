89fb4c9abaa92d0fd9bca47fcb11865ea668e0fc
Viktoriya (KochruovaVA) <KochruovaVA@mpei.ru>
2020-05-25 15:05:45 +0300

    head: добавление цветов

--- svg_module.h
+++ svg_module.h
@@ -1,8 +1,10 @@
 #pragma once
 
 #include <vector>
+#include <string>
 
 using namespace std;
 
-void show_histogram_svg(const vector<size_t> & bins);
+void show_histogram_svg(const vector<size_t> & bins, const vector<string> & colors);
+
 
